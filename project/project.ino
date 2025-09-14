78b85847b8ecb47e93d2134759105e4580b54205
L0puh <1opuhart@gmail.com>
2025-09-14 13:17:50 +0300

    turn on/off LED

--- project/project.ino
+++ project/project.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+#define BUTTON_PIN  2 
+#define LED_ON_PIN  3
+#define LED_OFF_PIN 4
 /* values for display printing */
 float last_update = 0.0f;
 const float sleep_time = 1000.0f;     // update time of display
@@ -11,19 +14,52 @@ void print_on_lcd(const char* text, int x, int y);
 void receive_message();
 void print_message();
 
+bool is_on = false;
+bool prev_state = false;
+float last_press = 0;
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_ON_PIN, OUTPUT);
+  pinMode(LED_OFF_PIN,OUTPUT);
 
   lcd.init();
   lcd.backlight();
   last_update = millis();
 }
 
+void button_turn_on(){
+  if (millis() - last_press > 300.0f) {
+    byte state = digitalRead(BUTTON_PIN);
+    if (state != prev_state) {
+      last_press = millis();
+      prev_state = state;
+      if (state == LOW) {
+        is_on = (is_on == HIGH) ? LOW: HIGH;
+      }
+    }
+  }
+
+  if (is_on){
+    digitalWrite(LED_ON_PIN, HIGH);
+    digitalWrite(LED_OFF_PIN, LOW);
+  } else {
+    digitalWrite(LED_OFF_PIN, HIGH);
+    digitalWrite(LED_ON_PIN, LOW);
+  }
+}
+
 void loop() {
-  print_message("some long text that\
-                probably doesn't fit in the something\
-                of this size and better it to split\
-                and show flowing...");
+  /* print_message("some long text that\ */
+  /*               probably doesn't fit in the something\ */
+  /*               of this size and better it to split\ */
+  /*               and show flowing..."); */
+
+  button_turn_on();
+  print_message("hello world");
+
 }
 
 
@@ -36,9 +72,8 @@ void receive_message() {
 void print_message(const char* text) {
   int len = strlen(text);
   if (len > 16) {
-  
   } else {
-    print_on_lcd(text, 0, 0);
+    print_on_lcd(text, 1, 0);
   }
   
 
